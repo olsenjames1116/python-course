c6498ddb0c09969bc048a07550e0f0d042628813
James Olsen <olsenjames1116@gmail.com>
2024-05-26 10:22:57 -0400

    Scope

---
+++ lesson11/scope.py
@@ -0,0 +1,22 @@
+name = "Dave"
+count = 1
+
+
+def greeting(firstname):
+    color = "blue"
+    print(color)
+    print(name)
+    print(firstname)
+
+
+greeting("John")
+
+
+def another():
+    greeting("Dave")
+    color = "blue"
+    count = 2
+    print(count)
+
+
+another()
